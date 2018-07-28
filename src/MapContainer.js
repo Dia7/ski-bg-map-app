0d3aaa3d01e9330ab78535b4f7ee53ff2d8ec756
Diana Ivanova Ratcheva <diana.ratcheva@gmail.com>
2018-07-28 11:19:43 +0300

    Adding Functionality and Content to the MapContainer Component

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,2 +1,44 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
+
+
+export default class MapContainer extends Component {
+
+    componentDidMount () {
+        this.mapLoad()
+    }
+
+     mapLoad() {
+    if (this.props && this.props.google) {
+      const {google} = this.props
+
+      const mapRef = this.refs.map
+      const node = ReactDOM.findDOMNode(mapRef)
+
+      const mapConfig = Object.assign({}, {
+        center: {lat: 41.968877, lng: 23.477193},
+        zoom: 10,
+        mapTypeId: 'terrain'
+      })
+
+      this.map = new google.maps.Map(node, mapConfig)
+
+    }
+
+  }
+
+    render() {
+        return (
+            <div>
+                <div className="map-container">
+                    <div className="sidebar"></div>
+                    <div className="theMap" role="application" ref="map">
+                    Loading map
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+
