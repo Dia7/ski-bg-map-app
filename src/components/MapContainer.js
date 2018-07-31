b1cda7f3723aa35d8bdfd094e0dc782292e3038b
Diana Ivanova Ratcheva <diana.ratcheva@gmail.com>
2018-07-31 15:37:31 +0300

    Implement 'FourSquare' Content to the InfoWindows

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -2,14 +2,67 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom'
 
 
+
 export default class MapContainer extends Component {
 
     state = {
-        locations: require('./Locations.json'),
+          locations: [
+        {
+            name: "Borovetz",
+            location: {lat: 42.270667, lng: 23.605616},
+            id: '5997070d6a60717704801060'
+        },
+        {
+            name: "Bezbog",
+            location: {lat: 41.733941, lng: 23.524224},
+            id: '4e1d712415207c4cbec540bd'
+        },
+        {
+            name: "Bansko",
+            location: {lat: 41.7809356, lng: 23.438783},
+            id: '4d4d4835fe7fb1f783595a42'
+        },
+        {
+            name: "Malyovitsa",
+            location: {lat: 42.166667, lng: 23.366667},
+            id: '4cc2f7d2c844721e0bd1df01'
+        },
+        {
+            name: "7_Rilski_Lakes",
+            location: {lat: 42.202778, lng: 23.32},
+            id: '4c4187fdda3dc9283609c9b9'
+        },
+        {
+            name: "Panichishte",
+            location: {lat: 42.263425, lng: 23.296852},
+            id: '4f33c8a9a17c3e7317e5f33f'
+        },
+        {
+            name: "Semkovo",
+            location: {lat: 42.046394, lng: 23.534161},
+            id: ''
+        },
+        {
+            name: "Kulinoto",
+            location: {lat: 41.865885, lng: 23.344038},
+            id: ''
+        },
+        {
+            name: "Kamenitza",
+            location: {lat: 41.768523, lng: 23.426883},
+            id: ''
+        },
+        {
+            name: "Kartala",
+            location: {lat: 42.043117, lng: 23.360738},
+            id: '51ee89e07dd2b876fb52cf2e'
+        }
+    ],
         markers: [],
         query: '',
+        keys: require('./keys.json'), //json file containing the api keys
         // globally declared the infoWindow
-        infowindow: new this.props.google.maps.InfoWindow({maxWidth: 200})
+        infowindow: new this.props.google.maps.InfoWindow({maxWidth: 250})
     }
 
     componentDidMount () {
@@ -56,6 +109,8 @@ export default class MapContainer extends Component {
                 lng: place.location.lng,
             },
             title: place.name,
+            photo: '',
+            id: place.id,
             animation: google.maps.Animation.DROP,
             map: this.map
         });
@@ -74,8 +129,8 @@ export default class MapContainer extends Component {
     populateInfoWindow(marker, infowindow){
 
         const {google} = this.props
-        //bounce when marker or listitems are clicked
 
+        //bounce when marker or listitems are clicked
         marker.setAnimation(google.maps.Animation.BOUNCE);
         setTimeout(function() {
             marker.setAnimation(null);
@@ -84,13 +139,79 @@ export default class MapContainer extends Component {
         if(infowindow.marker !== marker) {
             // is infowindow not already open on that current marker
             infowindow.marker = marker;
-            infowindow.setContent(`<h4>${marker.title}</h4><p>Bla bla</p>`);
+
+            //set the content of the info window from four square API
+            this.foursquareContent(marker, infowindow);
+            // infowindow.setContent(`<h4>${marker.title}</h4><p>Bla bla</p>`);
+            infowindow.setContent('');
             infowindow.addListener('closeInfo', function() {
                 infowindow.marker = null;
             })
             infowindow.open(this.map, marker)
         }
     }
+
+    foursquareContent(marker, infowindow) {
+
+    //set the request for the api
+    let request = `https://api.foursquare.com/v2/venues/search?` +
+    `client_id=${this.state.keys.ClientId}
+    &client_secret=${this.state.keys.ClientSecret}` +
+    `&ll=` + marker.getPosition().lat() +`,` + marker.getPosition().lng() +
+    `&v=20180527`;
+
+
+    //fetch the request using fitch API
+    fetch(request)
+    .then((response) => {
+      //check if the response was not successful
+      if (response.status !== 200) {
+        console.log('Request to foursquare was not successful!');
+        infowindow.setContent(`<strong>Error While Loading..</strong>`);
+      }
+      //otherwise successful
+      return response.json();
+    }).then((data) => {
+        if (data.response.venues){
+        //the list of venues returned by the search query.
+        var venues = data.response.venues;
+        //first venue
+        var theVenue = venues[0];
+        //properties of the first venue
+        var id = theVenue.id;
+        var name = theVenue.name;
+        var formattedAddress = '';
+
+        //construct the address from the array in formattedAddress JSON.
+        if (theVenue.location.formattedAddress) {
+          for (let line of theVenue.location.formattedAddress) {
+            formattedAddress += line + '</br>'; //new line in html to be used inside <p> element
+          }
+        }
+        //call for photos
+        // var imgurl = data.response.venue.photos.groups[0].items[0];
+
+           //check for prefix and suffix property, on successful return image
+            // if ( ((imgurl.hasOwnProperty('prefix')) && (imgurl.hasOwnProperty('suffix')))) {
+            //     marker.photo = imgurl.prefix + "200x100" + imgurl.suffix;
+            // } else {
+            //     marker.photo = 'Image not found';
+            // }
+
+        var foursquareUrl = `https://foursquare.com/v/${id}`;
+
+
+        infowindow.setContent(`<p><strong>Title: </strong>${name}</p>
+                <img src="+marker.photo+">
+                <p><strong>Address: </strong>${formattedAddress}</p>
+                <a href=${foursquareUrl}>Location on Foursquare</a>
+                `);
+      }
+    }).catch(exception => {
+      console.log(exception);
+    })
+    }
+
 
     changeLocation = () => {
         const {infowindow} = this.state
