5935a3260ba41ba2e35f70448f366662e826eb23
Diana Ivanova Ratcheva <diana.ratcheva@gmail.com>
2018-08-02 18:37:08 +0300

    Minor code structure changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import {GoogleApiWrapper} from 'google-maps-react';
 import './App.css';
 import MapContainer from './components/MapContainer.js'
-
-// import images
-import HamburgerIcon from './assets/hamburger_icon.png'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <a className="menu" tabIndex="0">
-          <img className="hamburger-icon" src={ HamburgerIcon } alt="hamburger" />
-        </a>
-        <h1 className="main-heading">Skiing in Bulgaria</h1>
+        <h1 className="main-heading" tabIndex="0" autoFocus={true}>The Best Ski Spots in Bulgaria</h1>
         <MapContainer google={this.props.google} />
       </div>
     );
