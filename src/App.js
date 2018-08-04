aae5a62614c6dc55324d1a4987fb62fdd375b9eb
Diana Ivanova Ratcheva <diana.ratcheva@gmail.com>
2018-08-04 12:12:10 +0300

    Adding Autorization Warning Functionality

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import {GoogleApiWrapper} from 'google-maps-react';
 import './App.css';
 import MapContainer from './components/MapContainer.js'
 
+
+// add Autorization warning massage
+window.gm_authFailure = () => {
+  const mapContainer = document.querySelector('.map-container');
+  mapContainer.innerHTML = '';
+  mapContainer.innerHTML = `
+  <div class='error-message'>
+    <h2 class='error-title'>
+      <span class='red-brackets'>{</span>
+        ERROR
+      <span class='red-brackets'>}</span>
+    </h2>
+    <p>Google Maps failed to load properly because an authorization problem.Check your browser console for more informations and how solve the problem. You can try also to refresh the page.<p>
+  </div>`;
+}
+
 class App extends Component {
   render() {
     return (
